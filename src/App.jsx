95250dae4e8c3b8553124d13ec5c17c617d632d3
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-07-24 17:33:35 +0600

    Basic preloader screen done

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,23 @@ function App() {
 
   return (
     <>
-    <h1 className="font-extrabold">Hello world</h1>
+    <div className="flex flex-col justify-center items-center min-h-screen !min-h-[100dvh]font-mulish">
+
+      <Preloader />
+      
+    </div>
     </>
   )
 }
 
 export default App
+
+function Preloader(){
+  return (
+    <>
+    <h1 className=" text-4xl font-bold mb-4">What to do today?
+    </h1>
+    <h1 className="text-6xl  animate-pulse font-bold">Thinking...</h1>
+    </>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+// Supports weights 200-900
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+const fontFamilyOverrides = {
+  'mulish': ['Mulish Variable', ...defaultTheme.fontFamily.sans],
+}
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: fontFamilyOverrides,
+    },
   },
   plugins: [],
 }
