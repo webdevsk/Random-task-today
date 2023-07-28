aca0078c7727ee466192bfc45d35c7eb6eebc0f2
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-07-28 14:56:27 +0600

    Done getting random activity

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState({})
+  const [error, setError] = useState(null)
 
+  useEffect(() => {
+    const controller1 = new AbortController()
+    // const controller2 = new AbortController()
+
+    const getData = async () => {
+      try {
+        const response1 = await axios.get('http://www.boredapi.com/api/activity/', {
+          signal: controller1.signal
+        })
+        const activity = await response1.data.activity
+
+        // const response2 = await axios.get()
+
+        setData({activity: activity})
+      } catch (error) {
+        error.code !== 'ERR_CANCELED' && setError(error)
+      } finally {
+        setLoading(false)
+      }
+      
+    }
+
+    getData()
+    return () => {
+      controller1.abort()
+      // controller2.abort()
+    }
+  }, [])
   return (
     <>
-    <div className="flex flex-col justify-center items-center min-h-screen !min-h-[100dvh]font-mulish">
-
-      <Preloader />
+    <div className="flex flex-col justify-center items-center min-h-screen min-h-[100dvh] font-mulish">
+    
+    {
+      loading
+      ? <Preloader />
+      : error
+      ? <ErrorMsg error={error}/>
+      : <RandomActivity data={data} />
+    }
       
+
     </div>
     </>
   )
@@ -22,5 +62,23 @@ function Preloader(){
     </h1>
     <h1 className="text-6xl  animate-pulse font-bold">Thinking...</h1>
     </>
+  )
+}
+
+
+
+function RandomActivity({data}){
+  console.log(data.activity)
+  return(
+    <>
+    <h1>{data.activity}</h1>
+    </>
+  )
+}
+
+function ErrorMsg({error}){
+  console.log(error)
+  return(
+    <h1 style={{color: 'red'}}>{error.message}</h1>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import '@fontsource-variable/mulish'
-
+console.log('Main mounted')
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
